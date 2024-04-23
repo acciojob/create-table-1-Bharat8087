d501b6572e6e5aaef36ad923d0654e5a3748767d
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-23 09:27:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 function insert_Row() {
     var table = document.getElementById('sampleTable');
-    var newRow = document.createElement('tr');
-    table.insertBefore(newRow, table.firstChild);
-    
-    var leftCell = document.createElement('td');
+    var newRow = table.insertRow(0);  // Insert a new row at the top of the table
+
+    var leftCell = newRow.insertCell(0);  // Insert a new cell in the new row at index '0'
     leftCell.innerText = 'New Cell1';
-    
-    var rightCell = document.createElement('td');
+
+    var rightCell = newRow.insertCell(1);  // Insert a new cell in the new row at index '1'
     rightCell.innerText = 'New Cell2';
-    
-    newRow.appendChild(leftCell);
-    newRow.appendChild(rightCell);
 }
