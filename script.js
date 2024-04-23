c8abf57d43a517ae6771693f4b5f93ecc422dce5
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-23 09:13:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
     //Write your code here
-  
+
+    var table = document.getElementById('sampleTable');
+    var newRow = table.insertRow();
+    var leftCell = newRow.insertCell(0);
+    var rightCell = newRow.insertCell(1);
+    leftCell.innerHTML = 'New Cell1';
+    rightCell.innerHTML = 'New Cell2';
+}
+
   
 }
